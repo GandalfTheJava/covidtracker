d6d2a98bcf666f18df5f80013178e2c45284203c
mehmet <mehmetcimen23@gmail.com>
2020-07-21 12:50:17 +0100

    Updates

---
+++ precache-manifest.db10a5a14f276c3a56b3783aea82781a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58a46db5663412d331b3a282944f020",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "a3809486619e28fefc26",
+    "url": "/covidtracker/static/css/main.f81fbfa0.chunk.css"
+  },
+  {
+    "revision": "5bcb4a8668b640ae8bf5",
+    "url": "/covidtracker/static/js/2.6587e654.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covidtracker/static/js/2.6587e654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3809486619e28fefc26",
+    "url": "/covidtracker/static/js/main.d8f84d14.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  }
+]);
